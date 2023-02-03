01b7aeb455dcc555723226d2457028a250aa3e43
Alexander Pankoff <ccntrq@screenri.de>
2023-02-03 23:14:05 +0100

    Add dentist character with keyboard movement

--- game.py
+++ game.py
@@ -8,11 +8,21 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Delirious Dentist"
 
+# scaling factor for the dentist character
+CHARACTER_SCALING = 0.1
+# movement speed of the dentist character
+CHARACTER_MOVEMENT_SPEED = 5
+
 
 class MyGame(arcade.Window):
-    """
-    Main application class.
-    """
+   """
+   Main application class.
+   """
+   def on_update(self, delta_time):
+        """Movement and game logic"""
+
+        # Move the player with the physics engine
+        self.physics_engine.update()
 
     def __init__(self):
 
@@ -21,15 +31,69 @@ class MyGame(arcade.Window):
 
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
+        self.player_list = None
+
+        # Our physics engine
+        # XXX currently unused
+        self.physics_engine = None
+
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
-        pass
+
+        """Set up the game here. Call this function to restart the game."""
+        # Create the Sprite lists
+        self.player_list = arcade.SpriteList()
+
+        # Set up the player, specifically placing it at these coordinates.
+        image_source = "resources/sprites/dentist.jpg"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
+
+        # Create the 'physics engine'
+        # XXX needs walls
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, arcade.SpriteList())
+
 
     def on_draw(self):
         """Render the screen."""
 
         self.clear()
         # Code to draw the screen goes here
+
+        self.player_list.draw()
+
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed."""
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = CHARACTER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = -CHARACTER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -CHARACTER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = CHARACTER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key."""
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time):
+        """Movement and game logic"""
+        # Move the player with the physics engine
+        self.physics_engine.update()
+
 
 
 def main():
