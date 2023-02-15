a7b408cc3ed3057ab45d4d28becdef89ff820ce2
Alexander Pankoff <ccntrq@screenri.de>
2023-02-15 18:44:23 +0100

    Refactor GrowAnimation

--- src/animation/grow.py
+++ src/animation/grow.py
@@ -4,12 +4,11 @@ import types
 class GrowAnimation:
     @staticmethod
     def animate(sprite):
+        def update_animation(self, delta_time: float = 1 / 60):
+            # TODO: add parameters for scaling
+            self.scale *= 1.2
+            maximum_scale = 6
+            if self.scale >= maximum_scale:
+                self.remove_from_sprite_lists()
+
         sprite.update_animation = types.MethodType(update_animation, sprite)
-
-
-def update_animation(self, delta_time: float = 1 / 60):
-	# TODO: add parameters for scaling
-    self.scale *= 1.2
-    maximum_scale = 6
-    if self.scale >= maximum_scale:
-        self.remove_from_sprite_lists()
